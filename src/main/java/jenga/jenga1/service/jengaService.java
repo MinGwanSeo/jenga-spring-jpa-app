7b0a63ee20c6617153e05a6549cc39b509a5e8f5
shp7408 <shp7408@naver.com>
2023-02-15 23:15:23 +0900

    프로젝트 전체 result 형태 변경

--- src/main/java/jenga/jenga1/service/jengaService.java
+++ src/main/java/jenga/jenga1/service/jengaService.java
@@ -1,7 +1,8 @@
 package jenga.jenga1.service;
 
+import jenga.jenga1.model.JengaVO;
 import java.util.Map;
 
 public interface jengaService {
-    Map<String, Object> getJengaData();
+    JengaVO getJengaData();
 }

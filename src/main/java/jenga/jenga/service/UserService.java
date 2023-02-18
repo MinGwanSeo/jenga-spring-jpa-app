c001f8f3909e91aadd42515f3d7c2e13d07b452a
shp7408 <shp7408@naver.com>
2023-02-19 01:52:10 +0900

    service 일단 비움

--- src/main/java/jenga/jenga/service/UserService.java
+++ src/main/java/jenga/jenga/service/UserService.java
@@ -1,8 +1,6 @@
 package jenga.jenga.service;
 
-import jenga.jenga.dto.UserDTO;
 
 public interface UserService {
-    UserDTO getUser(String userName);
 
 }
--- src/main/java/jenga/jenga/service/impl/UserServiceImpl.java
+++ src/main/java/jenga/jenga/service/impl/UserServiceImpl.java
@@ -1,27 +1,9 @@
 package jenga.jenga.service.impl;
 
-import jenga.jenga.model.User;
-import jenga.jenga.dto.UserDTO;
 import jenga.jenga.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.w3c.dom.UserDataHandler;
 
 @Service
 public class UserServiceImpl implements UserService {
 
-    UserDataHandler userDataHandler;
-
-    @Autowired
-    public UserServiceImpl(UserDataHandler userDataHandler) {
-        this.userDataHandler = userDataHandler;
-    }
-
-
-
-    @Override
-    public UserDTO getUser(String userName) {
-        User user = userDataHandler
-        return null;
-    }
 }
